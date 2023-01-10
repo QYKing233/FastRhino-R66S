9d11a23e42bde66a17541176b90f509ca89b9012
QYKing233 <61415390+QYKing233@users.noreply.github.com>
2023-01-10 23:33:20 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -57,4 +57,4 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 
 # Modify kernel version
-# sed -i 's/5.15/5.10/g' target/linux/rockchip/Makefile
+sed -i 's/5.15/6.1/g' target/linux/rockchip/Makefile
